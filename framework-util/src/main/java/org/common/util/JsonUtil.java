16863478630117d867a814dc41189bdf2afaa297
pujie <pujie@baiwutong.com>
2015-11-17 18:39:32 +0800

    add fastjson

---
+++ framework-util/src/main/java/org/common/util/JsonUtil.java
@@ -0,0 +1,58 @@
+package org.common.util;
+
+
+import java.io.IOException;
+
+import com.alibaba.fastjson.JSON;
+
+
+/**
+ * JSON与实体bean之间转换
+ * 
+ * @author liming
+ * @version 2013-07-22
+ * 
+ */
+public class JsonUtil {
+
+	/**
+	 * bean 2 JSON
+	 * 
+	 * @param obj
+	 * @return
+	 * @throws IOException
+	 */
+	public static String beanToJson(Object obj) throws IOException {
+		
+		String json =JSON.toJSONString(obj);
+		return json;
+	}
+
+	/**
+	 * JSON 2 BEAN
+	 * 
+	 * @param json
+	 * @param cls
+	 * @return
+	 * @throws Exception
+	 */
+    public static <T> T jsonToBean(String json, Class<T> cls) throws Exception {
+		T vo  = JSON.parseObject(json, cls);
+		return vo;
+	}
+    
+	public static void main(String[] args) throws Exception {
+		
+		
+//		// 准备数据
+//		Lecard lecard = new Lecard() ;
+//		lecard.setBatch("batch") ;
+//		lecard.setCardNumber("123456") ;
+//		// 实体转JSON字符串
+//		String json = beanToJson(lecard);
+//		System.out.println("bean 2 json:" + json);
+//
+//		Object limit2 = jsonToBean(json, Lecard.class);
+//		System.out.println(limit2.toString());
+	}
+}
